9cd51c73a89b703e99afc5adcc4fb0eb82c74eef
tendeanclaudio <69841294+tendeanclaudio@users.noreply.github.com>
2020-11-10 16:07:44 +0800

    exercise

---
+++ pertemuan_20/index.js
@@ -0,0 +1,62 @@
+//Variabel
+
+//let & const
+// let _umur = 25;
+// _umur = 26;
+// let nilai = 95.5;
+// let sudahMenikah = true;
+// let $nama = "Vini Vanisa";
+// console.log(_umur);
+// console.log($nama);
+
+// let alamat;
+// console.log(alamat);
+
+// const tahunLahir = 2010;
+// console.log(tahunLahir);
+
+//Operator
+
+//Aritmetic Operator (+,-,*,/)
+
+// let angka1 = 10;
+// let angka2 = 20;
+// console.log(angka1 + angka2);
+
+// let umur=30;
+// let namaDepan = "Vini";
+// let namaBelakang = "Vanisa"
+// console.log("Nama Saya " + namaDepan + " " + namaBelakang + ". Umur saya adalah" + umur + "tahun"
+// );
+
+//Operator perbandingan
+
+// let angka1 =10;
+// let angka2 =9;
+
+// console.log(angka1 <= angka2);
+
+
+//ternary operator
+// let hasil = 5;
+// let hasil = angka <= 0 ? "Negatif" : "Positif";
+// console.log(hasil);
+
+
+// //Operator Logika
+// let kondisi1 = true;
+// let kondisi2 = true;
+
+// console.log(kondisi1 || kondisi2);
+
+// console.log(!kondisi1);
+
+// let angka = promp("Masukan angka : ")
+// console.log("angka yang dimasukan adalah" + angka);
+
+//Exercise 7
+
+let berat = prompt("masukan Berat badan anda = kg");
+let tinggi = prompt("masukan Tinggi = cm");
+let bni = berat / (tinggi*tinggi);
+console.log("BMI = " + bni);
